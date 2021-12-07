514f059d4b643c4ed732ec758b3662abdf23dedd
Peterliang233 <ncuyanping666@126.com>
2021-12-07 13:49:31 +0800

    :poop: end

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Hello, World.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 
 def detail(request, question_id):
-    return HttpResponse("You're Looking the question: %s\n" % question_id)
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
-def result(request, question_id):
-    response = "You're Looking the question %s"
-    return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're Looking the question." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "you didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
